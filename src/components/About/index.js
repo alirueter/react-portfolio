eb163c06e3e59d22b76792054c765c8b4d688caa
alirueter <rueter.alison@gmail.com>
2020-10-09 12:32:16 -0500

    added cover image

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+    return (
+        <section>
+            <h1 id="about"> Who am I? </h1>
+            <img src={coverImage} style={{ width: "100%"}} alt="cover" />
+        </section>
+    )
+}
+
+export default About;
